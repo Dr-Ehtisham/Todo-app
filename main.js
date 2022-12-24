37cedee9ad83ed4701d6ab84e3e5c8983205a5ab
Dr-Ehtisham <91793628+Dr-Ehtisham@users.noreply.github.com>
2022-12-24 07:54:47 +0500

    Another event change is added

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const newTodoForm = document.querySelector('#new-todo-form');
 
 
 
+const username = localStorage.getItem('username') || '';
 
+nameInput.value = username;
+
+nameInput.addEventListener('change', e => {
+    localStorage.setItem('username', e.target.value);
+})
 
 
 
