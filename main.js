a0bd98bbe8372d079cddcdaa6a694cbcc580df5e
Dr-Ehtisham <91793628+Dr-Ehtisham@users.noreply.github.com>
2022-12-24 07:46:48 +0500

    Some elements got into JS from HTML

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', () =>{
-todos = JSON.parse(localStorage.getItem('todos')) || []
+todos = JSON.parse(localStorage.getItem('todos')) || [];
+const nameInput = document.querySelector('#name');
+const newTodoForm = document.querySelector('#new-todo-form');
+
+
+
+
+
+
+
 });
 
 
